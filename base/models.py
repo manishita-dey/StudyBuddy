ecca89b405e19521f4c87b3ae516cf6d779be658
Manishita Dey <manishita.dey@thoughts2binary.com>
2023-12-08 12:46:56 +0530

    New commit

--- base/models.py
+++ base/models.py
@@ -1,3 +1,43 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+
+class Topic(models.Model):
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+
+class Room(models.Model):
+    host = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    topic = models.ForeignKey(
+        Topic, on_delete=models.SET_NULL, null=True
+    )  # One topic can have many rooms
+    name = models.CharField(max_length=200)
+    description = models.TextField(null=True, blank=True)
+    # participants=
+    updated = models.DateTimeField(
+        auto_now=True
+    )  # will save everytime a change is made
+    created = models.DateTimeField(
+        auto_now_add=True
+    )  # will save the first time the room is created
+
+    def __str__(self):
+        return self.name
+
+
+class Message(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    room = models.ForeignKey(
+        Room, on_delete=models.CASCADE
+    )  # if one room gets deleted, all children mesaages get deleted. One Room can have many messages
+    body = models.TextField()
+    updated = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.body[0:50]  # only first 50 characters will be shown
--- base/views.py
+++ base/views.py
@@ -1,19 +1,27 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Room
 
-rooms = [
-    {"id": 1, "name": "Let's run python!"},
-    {"id": 2, "name": "Design with me!"},
-    {"id": 3, "name": "Frontend Developer!"},
-]
+# rooms = [
+#     {"id": 1, "name": "Let's run python!"},
+#     {"id": 2, "name": "Design with me!"},
+#     {"id": 3, "name": "Frontend Developer!"},
+# ]
 
 
 # Create your views here.
 def home(request):
+    rooms = Room.objects.all()
     context = {"rooms": rooms}
-    return render(request, "home.html", context)
+    return render(request, "base/home.html", context)
     # return render(request, "home.html", {"rooms": rooms})
 
 
-def room(request):
-    return render(request, "room.html")
+def room(request, pk):
+    # room = None
+    # for i in rooms:
+    #     if i["id"] == int(pk):
+    #         room = i
+    room = Room.objects.get(id=pk)
+    context = {"room": room}
+    return render(request, "base/room.html", context)
